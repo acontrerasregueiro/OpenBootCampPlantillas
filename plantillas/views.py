e5ea4437764218ed43a2f5f86bb01974eab10262
Adrian <acontrerasregueiro@gmail.com>
2022-09-13 11:27:27 +0200

    Empezando a usar plantillas con rutas dinamicas

--- plantillas/views.py
+++ plantillas/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def simple(request):
     return render(request,'simple.html',{})
+
+def dinamico(request,name):
+    #El contexto es un diccionario
+    contexto = {'name' : name}
+    return render(request,'dinamico.html',contexto)
+    
